018283730a7b31289ab3651e0651380089574000
noqcks <benji@093b.org>
2024-04-07 02:29:01 +0000

    Upgrade dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'typeface-roboto';
 import { Provider } from 'react-redux';
+import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
